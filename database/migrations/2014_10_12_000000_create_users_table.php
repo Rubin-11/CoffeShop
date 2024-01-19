89d752e7d848b53091b79bdb038ec3e710eeeb52
Sergey Misharin <misharin_sergey-work@mail.ru>
2024-01-19 08:56:05 +0300

    Базовый набор миграций

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,17 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->json('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('country');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -22,9 +22,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
---
+++ database/migrations/2024_01_19_044648_create_products_categories_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        // Продукты бренды
+        Schema::create('products_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('category');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products'); // Связь с продуктами
+            $table->foreign('category_id')->references('id')->on('categories'); // Связь с категориями
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products_categories');
+    }
+};
