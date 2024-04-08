db9cd6a13ad3a75a3e53523779fc8df3228cf4f0
Sergey Misharin <misharin_sergey-work@mail.ru>
2024-04-08 10:57:09 +0300

    H-2 сделал базовую верстку главной страницы

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/css/components/footer.css', 'resources/js/app.js','resources/css/pages/main.css'])
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-    </head>
-    <body>
-    @if (Route::has('login'))
-        <livewire:welcome.navigation />
-    @endif
-        {{ $slot }}
-    </body>
+    <title>{{ $title ?? 'Page Title' }}</title>
+</head>
+<body>
+<div class="container center">
+<livewire:components.header/>
+
+{{--    @if (Route::has('login'))--}}
+{{--        <livewire:welcome.navigation />--}}
+{{--    @endif--}}
+{{ $slot }}
+<livewire:components.footer/>
+</div>
+</body>
 </html>
