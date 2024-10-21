2509d3333976eb40b765e7877c5de0675d10e055
Sergey Misharin <misharin_sergey-work@mail.ru>
2024-10-21 12:32:09 +0300

    Реализация фильтра кофе по кислотности

--- storage/framework/views/7b4a4431620eb1adee7de3416d9d9e46.php
+++ storage/framework/views/7b4a4431620eb1adee7de3416d9d9e46.php
@@ -1,11 +1,21 @@
 <div>
     <h1>Каталог кофе</h1>
     <ul>
-        <!--[if BLOCK]><![endif]--><?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <!--[if BLOCK]><![endif]--><?php $__currentLoopData = $coffees; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $coffee): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <li>
-                <?php echo e($product->name); ?>
+                <?php echo e($coffee->name); ?>
 
             </li>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?><!--[if ENDBLOCK]><![endif]-->
     </ul>
+
+    <form method="GET" wire:submit="sortCoffeeAcidity">
+        <label for="mySelect">Выберите степень кислотности:</label>
+        <select wire:model='selectedAcidity' id="mySelect">
+            <!--[if BLOCK]><![endif]--><?php $__currentLoopData = $levels; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $level): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <option value="<?php echo e($level->id); ?>"><?php echo e($level->name); ?></option>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?><!--[if ENDBLOCK]><![endif]-->
+        </select>
+        <button type="submit">Отправить</button>
+    </form>
 </div><?php /**PATH /CoffeeShop/resources/views/livewire/pages/coffee-catalog.blade.php ENDPATH**/ ?>
